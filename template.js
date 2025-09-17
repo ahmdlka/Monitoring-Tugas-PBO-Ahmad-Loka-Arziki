3e27fa11fa9936573f11fb8e471de7a65a268309
Kaka <lokaarziki@gmail.com>
2025-09-17 13:23:06 +0700

    coba template 5.3 js

--- template.js
+++ template.js
@@ -2,23 +2,23 @@ class SpecialHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <!-- Header -->
-        <header>
-            <div class="container">
-                <h1>Dokumentasi Tugas PBO</h1>
-                <p>Ahmad Loka Arziki – 5025241044</p>
-            </div>
-        </header>
+      <header>
+          <div class="container">
+              <h1>Dokumentasi Tugas PBO</h1>
+              <p>Ahmad Loka Arziki – 5025241044</p>
+          </div>
+      </header>
 
-        <!-- Navigation -->
-        style="position: sticky;">
-            <div class="container nav-container">
-                <div class="logo">PBO Journal</div>
-                <div class="nav-links">
-                    <a href="index.html">Home</a>
-                    <a href="profil.html">Profil</a>
-                </div>
-            </div>
-        </nav>
+      <!-- Navigation -->
+      <nav style="position: sticky; top: 0; background: white; z-index: 1000;">
+          <div class="container nav-container">
+              <div class="logo">PBO Journal</div>
+              <div class="nav-links">
+                  <a href="index.html">Home</a>
+                  <a href="profil.html">Profil</a>
+              </div>
+          </div>
+      </nav>
     `;
   }
 }
