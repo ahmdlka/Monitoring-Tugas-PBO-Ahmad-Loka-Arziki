13aa654efa59275bf70a2c6558905e086819d9ba
Kaka <lokaarziki@gmail.com>
2025-09-17 13:03:26 +0700

    coba template 4 js

--- template.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('template.html')
-    .then(res => res.text())
-    .then(data => {
-      console.log("Isi template.html:", data); // debug
-      document.body.insertAdjacentHTML("afterbegin", data); // langsung tempel tanpa <template>
-    })
-    .catch(err => console.error("Fetch error:", err));
-});
